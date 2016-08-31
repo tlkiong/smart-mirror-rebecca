8f016d3d5736d582a876eaf3c33df160ed78ce23
Kiong <kiong90@gmail.com>
2016-09-01 04:18:04 +0800

    Done with base structure

--- www/modules/dashboard/dashboard.controller.js
+++ www/modules/dashboard/dashboard.controller.js
@@ -4,10 +4,11 @@
     angular.module('Dashboard')
         .controller('dashboardController', dashboardController);
 
-    dashboardController.$inject = ['commonService', 'dashboardService'];
+    dashboardController.$inject = ['$mdDialog', 'commonService', 'dashboardService'];
 
-    function dashboardController(commonService, dashboardService) {
+    function dashboardController($mdDialog, commonService, dashboardService) {
       var vm = this;
+      vm.openMediaPlayer = openMediaPlayer;
 
       /* ======================================== Var ==================================================== */
       vm.misc = {};
@@ -15,10 +16,39 @@
       /* ======================================== Services =============================================== */
       var svc = dashboardService;
       var cmnSvc = commonService;
+      var mdDialog = $mdDialog;
 
       /* ======================================== Public Methods ========================================= */
+      function openMediaPlayer(ev) {
+        mdDialog.show({
+          controller: DialogController,
+          templateUrl: 'modules/dashboard/mediaPlayer.html',
+          parent: angular.element(document.body),
+          targetEvent: ev,
+          clickOutsideToClose:true,
+          fullscreen: true
+        })
+        .then(function(answer) {
+          $scope.status = 'You said the information was "' + answer + '".';
+        }, function() {
+          $scope.status = 'You cancelled the dialog.';
+        });
+      }
+
 
       /* ======================================== Private Methods ======================================== */
+      function DialogController($scope, $mdDialog) {
+        $scope.hide = function() {
+          $mdDialog.hide();
+        };
+        $scope.cancel = function() {
+          $mdDialog.cancel();
+        };
+        $scope.answer = function(answer) {
+          $mdDialog.hide(answer);
+        };
+      }
+
       function init() {
           
       }
