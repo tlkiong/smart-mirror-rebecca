b12a8506d0b02290707c72de2644fbb978fcd843
Kiong <kiong90@gmail.com>
2016-09-01 15:23:45 +0800

    Able to track face but extremely slow

--- www/modules/dashboard/dashboard.controller.js
+++ www/modules/dashboard/dashboard.controller.js
@@ -57,6 +57,54 @@
 
 
       /* ======================================== Private Methods ======================================== */
+      function getVideo() {
+        var video = document.getElementById('video');
+        var canvas = document.getElementById('canvas');
+        var context = canvas.getContext('2d');
+        var tracker = new tracking.ObjectTracker('face');
+        tracker.setInitialScale(4);
+        tracker.setStepSize(2);
+        tracker.setEdgesDensity(0.1);
+        tracking.track('#video', tracker, { camera: true });
+        tracker.on('track', function(event) {
+          context.clearRect(0, 0, canvas.width, canvas.height);
+          event.data.forEach(function(rect) {
+            context.strokeStyle = '#a64ceb';
+            context.strokeRect(rect.x, rect.y, rect.width, rect.height);
+            context.font = '11px Helvetica';
+            context.fillStyle = "#fff";
+            context.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
+            context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
+          });
+        });
+        var gui = new dat.GUI();
+        gui.add(tracker, 'edgesDensity', 0.1, 0.5).step(0.01);
+        gui.add(tracker, 'initialScale', 1.0, 10.0).step(0.1);
+        gui.add(tracker, 'stepSize', 1, 5).step(0.1);
+      }
+
+      function loadCam() {
+        var deferred = cmnSvc.$q.defer();
+
+        var video = document.querySelector("#video");
+ 
+        navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
+         
+        if (navigator.getUserMedia) {       
+          navigator.getUserMedia({
+            video: true
+          }, function handleVideo(stream) {
+              video.src = window.URL.createObjectURL(stream);
+              deferred.resolve();
+          }, function videoError(e) {
+              // do something
+              deferred.reject();
+          });
+        }
+
+        return deferred.promise;
+      }
+
       function getWeatherInformation() {
         httpReqSvc.http('openWeatherApi', undefined, vm.misc.weatherApiDataObj).then(function(rs) {
           vm.widget.weather.temperature = rs.data.main.temp;
@@ -152,6 +200,10 @@
             getWeatherInformation();
           }, numberOfMinutes);
         }, function(er) {});
+
+        loadCam().then(function(rs){
+          getVideo();
+        });
       }
 
       init();
